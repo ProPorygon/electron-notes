db123bef8b2a4261901f23b67142e2cc85167725
Harrison Qu <harrisonqu96@gmail.com>
2016-02-20 16:38:08 -0600

    added id

--- calls.js
+++ calls.js
@@ -1,10 +1,11 @@
-function callAPI() {
+function callAPI(inString) {
     var AlchemyAPI = require('alchemy-api');
     var alchemy = new AlchemyAPI('5e0a4198d583aabe8a39557d240670120974a254');
     var Client = require('node-wolfram');
     var Wolfram = new Client('WGTR76-VGTY7HUV2X');
-    alchemy.keywords('Glucose is comprised of 6 carbons, 12 hydrogens, and 6 oxygens.', {}, function (err, response) {
+    alchemy.keywords(inString, {}, function (err, response) {
         if (err) throw err;
+        var sidebar = document.getElementById("side");
 
         // See http://www.alchemyapi.com/api/keyword/htmlc.html for format of returned object
         var keywords = response.keywords;
@@ -23,6 +24,10 @@ function callAPI() {
                             for (var c = 0; c < subpod.plaintext.length; c++) {
                                 var text = subpod.plaintext[c];
                                 console.log('\t', text);
+                                var innerDiv = document.createElement('div');
+                                innerDiv.className = 'block-2';
+                                sidebar.appendChild(innerDiv);
+                                innerDiv.innerHTML = text;
                             }
                         }
                     }
