3b6dd66cbf4b661bd1429ce78f7c5c230f34a794
Harrison Qu <harrisonqu96@gmail.com>
2016-02-20 17:26:56 -0600

    hi

--- calls.js
+++ calls.js
@@ -6,25 +6,31 @@ function callAPI(inString) {
     alchemy.keywords(inString, {}, function (err, response) {
         if (err) throw err;
         var sidebar = document.getElementById("side");
-        sidebar.innerHTML = "";
         // See http://www.alchemyapi.com/api/keyword/htmlc.html for format of returned object
         var keywords = response.keywords;
-        console.log(keywords);
         if(keywords == null || keywords.length == 0){
             alert("No keywords found!");
             return;
         }
+        sidebar.innerHTML = "";
         for (var idx = 0; idx < keywords.length; idx++) {
             var qu = keywords[idx].text;
             Wolfram.query(qu, function (err, result) {
                 if (err)
                     console.log(err);
                 else {
+                    console.log(result);
+                    console.log(result.queryresult.pod)
+                    if(result.queryresult.pod == null){
+                        alert("No Results Found");
+                        return;
+                    }
                     for (var a = 0; a < result.queryresult.pod.length; a++) {
                         var pod = result.queryresult.pod[a];
                         console.log(pod.$.title, ": ");
                         for (var b = 0; b < pod.subpod.length; b++) {
                             var subpod = pod.subpod[b];
+                            console.log("hi");
                             for (var c = 0; c < subpod.plaintext.length; c++) {
                                 var text = subpod.plaintext[c];
                                 console.log('\t', text);
