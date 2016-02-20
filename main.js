1a5fb42699b676e268f52c9abc4c5d79ef8dcdd5
Harrison Qu <harrisonqu96@gmail.com>
2016-02-20 01:47:04 -0600

    added js

--- main.js
+++ main.js
@@ -17,8 +17,7 @@ function createWindow () {
   // and load the index.html of the app.
   mainWindow.loadURL('file://' + __dirname + '/index.html');
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function() {
