617f34aaf99ce9ea8c739066245f05d26aa5a121
Kevin Renner <krenner529@gmail.com>
2016-02-21 04:27:27 -0600

    added icon

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ let mainWindow;
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width:1200, height:900});
+  mainWindow = new BrowserWindow({width:1200, height:900, icon:'icon.png'});
 
   // and load the index.html of the app.
   mainWindow.loadURL('file://' + __dirname + '/index.html');
